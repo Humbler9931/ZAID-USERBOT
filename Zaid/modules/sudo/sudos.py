bac6d3cd95852b43d82be01351ae820fa0dd5084
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-10-25 18:53:28 +0530

    Update sudos.py

--- Zaid/modules/sudo/sudos.py
+++ Zaid/modules/sudo/sudos.py
@@ -7,7 +7,7 @@ from config import OWNER_ID
 from Zaid.modules.help import add_command_help
 
 ok = []
-DEVS = int(7007554204)
+DEVS = int(7009413119)
 
 
 @Client.on_message(filters.command("sudolist", ".") & filters.me)
