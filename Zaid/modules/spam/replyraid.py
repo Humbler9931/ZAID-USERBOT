c5caa53b759dd943aac2c2b2e05933f2397f19d5
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-11-29 19:03:02 +0530

    Update replyraid.py

--- Zaid/modules/spam/replyraid.py
+++ Zaid/modules/spam/replyraid.py
@@ -129,42 +129,6 @@ add_command_help(
 )
 
 @Client.on_message(
-    filters.command(["replyraid"], ".") & (filters.me | filters.user(SUDO_USER))
-)
-async def add_reply_raid(client, message):
-    try:
-        aux = await message.reply("**🔄 Processing ...**")
-        if not message.reply_to_message:
-            if len(message.command) != 2:
-                return await aux.edit(
-                    "**🤖 Reply to a user's message or give username/user_id.**"
-                )
-            user = message.text.split(None, 1)[1]
-            if "@" in user:
-                user = user.replace("@", "")
-            fulluser = await client.get_users(user)
-            user_id = fulluser.id
-        else:
-            user_id = message.reply_to_message.from_user.id
-
-        if user_id == message.from_user.id:
-            return await aux.edit(
-                "**🤣 How Fool, You Want To Activate Reply Raid On Your Own ID❓**"
-            )
-        
-        fraid = await add_fuckraid_user(user_id)
-        if fraid:
-            return await aux.edit(
-                "**🤖 Successfully Added Reply Raid On This User.**"
-            )
-        return await aux.edit(
-            "**🤖 Hey, Reply Raid Already Active On This User❗**"
-        )
-    except Exception as e:
-        print(f"Error: {e}")
-        await message.reply(f"**🚨 Error:** `{e}`")
-
-@Client.on_message(
     filters.command(["dreplyraid"], ".") & (filters.me | filters.user(SUDO_USER))
 )
 async def gmute_user(client: Client, message: Message):
